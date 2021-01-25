55f6271772d5c7ddc908c6589f1ac32b6ea4f6f7
m4k5 <27677566+m4k5@users.noreply.github.com>
2021-01-25 10:41:20 +0100

    fix

--- GameOfLifeWebApp/settings.py
+++ GameOfLifeWebApp/settings.py
@@ -128,4 +128,5 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 django_heroku.settings(locals())
