e5ad15fe1b50887b4428caed6ce6b5e487f602de
m4k5 <27677566+m4k5@users.noreply.github.com>
2021-01-25 10:17:18 +0100

    pipenv missing gunicorn and django-heroku

--- GameOfLifeWebApp/settings.py
+++ GameOfLifeWebApp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,4 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals())
