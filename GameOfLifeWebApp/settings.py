2a0faada9e4e9727cbc626bd1b2bb700805f5b0a
m4k5 <27677566+m4k5@users.noreply.github.com>
2021-01-25 10:38:07 +0100

    cors

--- GameOfLifeWebApp/settings.py
+++ GameOfLifeWebApp/settings.py
@@ -127,5 +127,5 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 django_heroku.settings(locals())
